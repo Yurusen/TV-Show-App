bd91ca24fd808ccd053555db103f914ed1626732
Ajay Sasan <135863670+Yurusen@users.noreply.github.com>
2023-12-29 10:48:59 +0000

    Delete api directory

--- api/config.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "?api_key=cb3a79636c17c0245b17a9bee90d64e3";
-const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
-const SMALL_IMAGE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
-
-export { BASE_URL, API_KEY, BACKDROP_BASE_URL, SMALL_IMAGE_COVER_BASE_URL };
