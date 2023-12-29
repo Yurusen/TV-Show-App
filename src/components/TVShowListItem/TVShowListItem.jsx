8361de0f8d44d8642c2de143f1e3b71337f9502b
Ajay Sasan <135863670+Yurusen@users.noreply.github.com>
2023-12-29 10:57:11 +0000

    Create TVShowListItem.jsx

---
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -0,0 +1,25 @@
+import s from "./style.module.css";
+import { SMALL_IMAGE_COVER_BASE_URL } from "../../api/config";
+
+const MAX_TITLE_CHAR = 20;
+
+export function TVShowListItem({ tvShow, onClick }) {
+  const onClick_ = () => {
+    onClick(tvShow);
+  };
+
+  return (
+    <div onClick={onClick_} className={s.container}>
+      <img
+        alt={tvShow.title}
+        src={SMALL_IMAGE_COVER_BASE_URL + tvShow.backdrop_path}
+        className={s.img}
+      />
+      <div className={s.title}>
+        {tvShow.title.length > MAX_TITLE_CHAR
+          ? tvShow.title.slice(0, MAX_TITLE_CHAR) + "..."
+          : tvShow.title}
+      </div>
+    </div>
+  );
+}
